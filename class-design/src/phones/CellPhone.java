fc737d3a0727ae2557e9b08c4d873e77b31eb727
Josh Archer <jarcher@greenriver.edu>
2024-01-19 08:54:31 -0800

    Added a CellPhone class from the video lecture.

---
+++ class-design/src/phones/CellPhone.java
@@ -0,0 +1,74 @@
+package phones;
+
+public class CellPhone
+{
+    private String brand;
+    private int volume;
+    private String chargingPort;
+    private double storageInGigs;
+
+    public CellPhone(String brand, int volume,
+                     String chargingPort, double storageInGigs)
+    {
+        this.brand = brand;
+        this.volume = volume;
+        this.chargingPort = chargingPort;
+        this.storageInGigs = storageInGigs;
+    }
+
+    public String getBrand()
+    {
+        return brand;
+    }
+
+    public int getVolume()
+    {
+        return volume;
+    }
+
+    public String getChargingPort()
+    {
+        return chargingPort;
+    }
+
+    public double getStorageInGigs()
+    {
+        return storageInGigs;
+    }
+
+    public void setBrand(String brand)
+    {
+        this.brand = brand;
+    }
+
+    public void setVolume(int volume)
+    {
+        if (volume >= 0 && volume <= 10)
+        {
+            this.volume = volume;
+        }
+    }
+
+    public void setChargingPort(String chargingPort)
+    {
+        this.chargingPort = chargingPort;
+    }
+
+    public void setStorageInGigs(double storageInGigs)
+    {
+        this.storageInGigs = storageInGigs;
+    }
+
+    public void answerPhone(String incomingNumber)
+    {
+        System.out.println("You pick up the call from " + incomingNumber);
+        System.out.println("You answered on a " + brand + " phone");
+        this.toString();
+    }
+
+    //this converts the object to a string (and returns the string)
+    public String toString()
+    {
+        return "CellPhone - " + brand;
+    }
+}
---
+++ class-design/src/phones/Program.java
@@ -0,0 +1,9 @@
+package phones;
+
+public class Program
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
