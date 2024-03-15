4e629529949247b3360fd607ed48c1c00070de88
Josh Archer <jarcher@greenriver.edu>
2024-03-15 09:50:43 -0700

    Added an example of using methods from Java's map types.

--- src/lists/ListsPractice.java
+++ src/lists/ListsPractice.java
@@ -7,6 +7,32 @@ public class ListsPractice
 {
     public static void main(String[] args)
     {
+        ArrayList<Double> listOfNums = new ArrayList<Double>();
+
+        //store 1,000,000 random numbers
+        for (int i = 1; i <= 1_000_000; i++)
+        {
+            double randNum = Math.random();
+            listOfNums.add(Math.random());
+        }
+
+        int numElements = listOfNums.size();
+        for (int i = 0; i < numElements; i++)
+        {
+            listOfNums.remove(0);
+        }
+
+        for (int i = 0; i < listOfNums.size(); i++)
+        {
+            double number = listOfNums.get(i);
+        }
+
+
+        System.out.println("Removed all elements!");
+    }
+
+    private static void foo2()
+    {
         int[] numbers = {34, 11, 77, 32, 42, 13, 64, 21, 68};
 
         //is the number 42 in the array?
@@ -37,6 +63,12 @@ public class ListsPractice
         wordsList.add("hullaballoo");
         wordsList.add("whippersnapper");
         wordsList.add("biblioklept");
+
+        //syntactic sugar (there is an underlying mechanism - called an iterator)
+        for (String word : wordsList)
+        {
+            System.out.println("Found a word - " + word);
+        }
 
         boolean containsWord = wordsList.contains("lollygag");
         if (containsWord)
